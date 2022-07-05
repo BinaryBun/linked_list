4792c5f8e8086d6c7553b8e6d4a21714b21f6113
BinaryBun <chibishew2018@gmail.com>
2022-07-05 15:57:04 +0300

    list pop

--- linked_list.go
+++ linked_list.go
@@ -11,7 +11,7 @@ type List struct {
   head *Node
 }
 
-func (l *List) addBack(num int) {
+func (l *List) addBack (num int) {
   new_data := &Node{value: num}
   if l.head == nil {
     l.head = new_data
@@ -21,6 +21,30 @@ func (l *List) addBack(num int) {
       current = current.next
     }
     current.next = new_data
+  }
+}
+
+func (l *List) addFront (num int) {
+  new_data := &Node{value: num}
+  if l.head != nil {
+    new_data.next = l.head
+    l.head = new_data
+  } else {
+    l.head = new_data
+  }
+}
+
+func (l *List) popBack () {
+  current := l.head
+  for current.next.next != nil {
+    current = current.next
+  }
+  current.next = nil
+}
+
+func (l *List) popFront () {
+  if l.head != nil {
+    l.head = l.head.next
   }
 }
 
@@ -41,5 +65,9 @@ func main() {
   data.addBack(14)
   data.addBack(5)
   data.addBack(20)
+  data.addFront(22)
+  data.addFront(28)
+  data.popBack()
+  data.popFront()
   data.print()
 }
